0c10cd01448e88eac1d1302cda619f2ddf50b16b
Knut Wannheden <knut.wannheden@avaloq.com>
2018-02-23 11:55:44 +0100

    QA: Fix unused import in ValidationHelper

--- com.avaloq.tools.ddk.xtext.test.core/src/com/avaloq/tools/ddk/xtext/test/validation/ValidationHelper.java
+++ com.avaloq.tools.ddk.xtext.test.core/src/com/avaloq/tools/ddk/xtext/test/validation/ValidationHelper.java
@@ -13,7 +13,6 @@ package com.avaloq.tools.ddk.xtext.test.validation;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.junit4.validation.ValidationTestHelper;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.validation.CheckMode;
@@ -37,7 +36,7 @@ public class ValidationHelper extends ValidationTestHelper {
   }
 
   /**
-   * Assert no {@link Resource#getErrors() syntax or linking errors} exist in the resource of the given object.
+   * Assert no {@link org.eclipse.emf.ecore.resource.Resource#getErrors() syntax or linking errors} exist in the resource of the given object.
    *
    * @param obj
    *          the object to test for errors
