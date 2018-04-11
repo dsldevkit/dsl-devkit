4f332b4570a6ddb18c8885c39115f87185fc66b0
Roman Mitin <roman.mitin@avaloq.com>
2018-04-11 16:33:18 +0200

    Split associations access API from associations store API
    This will allow reusing access API for xtext index extension in a
    DB-backed implementation without mixing it with the store API which is a
    platform extension.
    
    Change-Id: I435bbd67152cd0610c30bfcb3f1afb73aa415534

--- com.avaloq.tools.ddk.xtext.builder/src/com/avaloq/tools/ddk/xtext/builder/IDerivedObjectAssociationsStore.java
+++ com.avaloq.tools.ddk.xtext.builder/src/com/avaloq/tools/ddk/xtext/builder/IDerivedObjectAssociationsStore.java
@@ -11,39 +11,9 @@
 
 package com.avaloq.tools.ddk.xtext.builder;
 
-import java.util.Set;
-
-import org.eclipse.emf.common.util.URI;
-
-
 /**
- * Store API for associations between resources processed by the builder and the objects (i.e. sources)
- * derived by various providers (i.e. by builder participants) for those resources.
- * <p>
- * Read more about associations in {@see DerivedObjectAssociations}.
- * </p>
+ * Store API for associations used as an extension for platform.
  */
-public interface IDerivedObjectAssociationsStore {
-
-  /**
-   * Update associated derived artifacts for the given generator and resource.
-   *
-   * @param resourceUri
-   *          URI identifying the resource processed by the builder, the URI must be known to the Xtext index
-   * @param generatorType
-   *          the name of the generator that processed the resource
-   * @param derivedObjectUris
-   *          URIs identifying the generated objects
-   */
-  void registerAssociations(URI resourceUri, String generatorType, Set<String> derivedObjectUris);
-
-  /**
-   * Returns associations for objects derived for the given resource identified by a resource URI.
-   *
-   * @param resourceUri
-   *          the resource URI, must not be {@code null}
-   * @return the associations, never {@code null} for existing sources, or {@code null} if the passed URI is not known to the builder
-   */
-  DerivedObjectAssociations getAssociations(URI resourceUri);
+public interface IDerivedObjectAssociationsStore extends IDerivedObjectAssociationsAccess {
 
 }
