b23a9549aae8766531eae53c92c962106b1f05df
Roman Mitin <roman.mitin@avaloq.com>
2018-08-24 15:36:53 +0200

    Derelease retry in indexing if reference resolution fails
    DSLs should not rely on indexing order and if needed use other means to
    resolve within indexing phase (i.e. include-like concepts over file
    system directly).
    
    So we are replacing an old mechanism that retries indexing of a source
    if it attempts to resolve a reference in an indexing phase and fails
    with a warning.
    
    Now resource descriptions will be written AS IS in case of a failed
    linking attempt. A warning log message will be reported, meaning that
    DSL-implementers need to do something.

--- com.avaloq.tools.ddk.xtext.builder/src/com/avaloq/tools/ddk/xtext/builder/Messages.java
+++ com.avaloq.tools.ddk.xtext.builder/src/com/avaloq/tools/ddk/xtext/builder/Messages.java
@@ -19,12 +19,11 @@ public class Messages extends NLS {
   public static String MonitoredClusteringBuilderState_CANNOT_LOAD_RESOURCE;
   public static String MonitoredClusteringBuilderState_NO_MORE_RESOURCES;
   public static String MonitoredClusteringBuilderState_PHASE_ONE_DONE;
-  public static String MonitoredClusteringBuilderState_TRY_AGAIN;
-  public static String MonitoredClusteringBuilderState_TRY_AGAIN_FAILED;
   public static String MonitoredClusteringBuilderState_UPDATE_DESCRIPTIONS;
   public static String MonitoredClusteringBuilderState_WRITE_DESCRIPTIONS;
   public static String MonitoredClusteringBuilderState_WRITE_ONE_DESCRIPTION;
   public static String MonitoredClusteringBuilderState_COULD_NOT_PROCESS_DUE_TO_STACK_OVERFLOW_ERROR;
+  public static String MonitoredClusteringBuilderState_FAILED_REFERENCE_RESOLUTION_IN_INDEXING;
 
   static {
     // initialize resource bundle
