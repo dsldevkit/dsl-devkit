3b640e1702c943702ba46f1ef73e9f4ed5b6a5da
Rubén Porras Campo <Ruben.PorrasCampo@avaloq.com>
2019-10-01 13:42:02 +0200

    Expose the delegate of ForwardingMap used in ResourceDescriptionStrategy
    Expose the delegate of ForwardingMap used in the generated code of the
    ResourceDescriptionStrategy. This allows consumers of this map to get a
    reference to the ImmutableMap. Until now consumers who needed such an
    immutable map where creating a new immutable map which was a copy of the
    original one. This overhead can be eliminated by exposing the delegate.
    
    Additionally, an unused import in the class TraceSet has been removed.

---
+++ com.avaloq.tools.ddk.xtext/src/com/avaloq/tools/ddk/xtext/resource/extensions/AbstractForwardingResourceDescriptionStrategyMap.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Avaloq Evolution AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Avaloq Evolution AG - initial API and implementation
+ *******************************************************************************/
+
+package com.avaloq.tools.ddk.xtext.resource.extensions;
+
+import java.util.Map;
+
+import com.google.common.collect.ForwardingMap;
+import com.google.common.collect.ImmutableMap;
+
+
+/**
+ * The Class AbstractForwardingResourceDescriptionStrategyMap.
+ */
+public abstract class AbstractForwardingResourceDescriptionStrategyMap extends ForwardingMap<String, String> {
+
+  private Map<String, String> delegateMap;
+
+  /**
+   * Implementations must use this method to associates keys with value by using the method {@code put} of the {@link ImmutableMap.Builder}.
+   *
+   * @param builder
+   *          the builder
+   */
+  protected abstract void fill(final ImmutableMap.Builder<String, String> builder);
+
+  @Override
+  public Map<String, String> delegate() {
+    if (delegateMap == null) {
+      ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+      fill(builder);
+      delegateMap = builder.build();
+    }
+
+    return delegateMap;
+  }
+
+}
