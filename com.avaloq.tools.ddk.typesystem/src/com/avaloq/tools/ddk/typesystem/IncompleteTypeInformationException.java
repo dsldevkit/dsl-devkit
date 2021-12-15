f55d6bce6828d6d40bda4afbad12e4c8f8942c11
rubenporras <43636626+rubenporras@users.noreply.github.com>
2021-12-15 10:10:48 +0100

    Fix syntax in IncompleteTypeInformationException

--- com.avaloq.tools.ddk.typesystem/src/com/avaloq/tools/ddk/typesystem/IncompleteTypeInformationException.java
+++ com.avaloq.tools.ddk.typesystem/src/com/avaloq/tools/ddk/typesystem/IncompleteTypeInformationException.java
@@ -18,7 +18,7 @@ package com.avaloq.tools.ddk.typesystem;
  */
 public class IncompleteTypeInformationException extends Exception {
 
-  private static final long serialVersionUID = 5032053653991144734L;IncompleteTypeInformationException
+  private static final long serialVersionUID = 5032053653991144734L;
 
   /**
    * Creates a new instance of {@link IncompleteTypeInformationException} with the given message.
