030bcc739cdf3221e8762366a6ba6bd70c8b650e
rubenporras <43636626+rubenporras@users.noreply.github.com>
2023-11-01 14:59:37 +0100

    Remove unused import (#818)

--- com.avaloq.tools.ddk.check.core.test/src/com/avaloq/tools/ddk/check/core/test/BugAig1314.java
+++ com.avaloq.tools.ddk.check.core.test/src/com/avaloq/tools/ddk/check/core/test/BugAig1314.java
@@ -13,7 +13,6 @@ package com.avaloq.tools.ddk.check.core.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.InputStream;
 import java.net.MalformedURLException;
