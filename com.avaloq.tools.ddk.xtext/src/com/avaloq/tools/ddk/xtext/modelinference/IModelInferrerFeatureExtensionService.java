133a75b3289f318d0dd40d95a7a1bce98a4d32c3
João Dinis Ferreira <hello@joaof.eu>
2025-10-18 14:27:16 +0200

    chore: rename 'Avaloq Licence AG' --> 'Avaloq Group AG'

--- com.avaloq.tools.ddk.xtext/src/com/avaloq/tools/ddk/xtext/modelinference/IModelInferrerFeatureExtensionService.java
+++ com.avaloq.tools.ddk.xtext/src/com/avaloq/tools/ddk/xtext/modelinference/IModelInferrerFeatureExtensionService.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) Avaloq Licence AG
+ * Copyright (c) Avaloq Group AG
  * Schwerzistrasse 6, 8807 Freienbach, Switzerland, http://www.avaloq.com
  * All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Avaloq Licence AG.
+ * This software is the confidential and proprietary information of Avaloq Group AG.
  * You shall not disclose whole or parts of it and shall use it only in accordance with the terms of the
- * licence agreement you entered into with Avaloq Licence AG.
+ * licence agreement you entered into with Avaloq Group AG.
  */
 
 package com.avaloq.tools.ddk.xtext.modelinference;
@@ -21,4 +21,4 @@ public interface IModelInferrerFeatureExtensionService extends IModelInferrer {
 
 }
 
-/* Copyright (c) Avaloq Licence AG */
+/* Copyright (c) Avaloq Group AG */
