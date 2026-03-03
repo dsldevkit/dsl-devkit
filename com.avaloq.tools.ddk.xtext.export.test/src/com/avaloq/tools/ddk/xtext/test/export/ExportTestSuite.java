5b9f831aa4392ec24141257c6538cde3d30723d3
João Dinis Ferreira <hello@joaof.eu>
2026-03-03 21:49:12 +0100

    fix: harden ExportGeneratorX package derivation

--- com.avaloq.tools.ddk.xtext.export.test/src/com/avaloq/tools/ddk/xtext/test/export/ExportTestSuite.java
+++ com.avaloq.tools.ddk.xtext.export.test/src/com/avaloq/tools/ddk/xtext/test/export/ExportTestSuite.java
@@ -13,6 +13,7 @@ package com.avaloq.tools.ddk.xtext.test.export;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+import com.avaloq.tools.ddk.xtext.export.generator.ExportGeneratorXTest;
 import com.avaloq.tools.ddk.xtext.export.exporting.ExportExportingTest;
 import com.avaloq.tools.ddk.xtext.export.formatting.ExportFormattingTest;
 import com.avaloq.tools.ddk.xtext.export.scoping.ExportScopingTest;
@@ -23,6 +24,6 @@ import com.avaloq.tools.ddk.xtext.export.validation.ExportValidationTest;
  * Empty class serving only as holder for JUnit4 annotations.
  */
 @Suite
-@SelectClasses({ExportFormattingTest.class, ExportValidationTest.class, ExportScopingTest.class, ExportExportingTest.class})
+@SelectClasses({ExportFormattingTest.class, ExportValidationTest.class, ExportScopingTest.class, ExportExportingTest.class, ExportGeneratorXTest.class})
 public class ExportTestSuite {
 }
